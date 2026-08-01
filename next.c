57e93689d8eba7e5a6c467a96632716c8d0d61a1
Tabassum Ridy <tabassumridy461@gmail.com>
2026-08-01 12:59:03 +0600

    Add list of C programming topics and files

---
+++ next.c
@@ -0,0 +1,27 @@
+No.	File Name	Topic	Concepts
+16	16_ArrayInputOutput.c	Array Input & Output	Arrays, Loops
+17	17_ArraySumAverage.c	Sum & Average of Array Elements	Arrays, Arithmetic
+18	18_ArrayMaximumMinimum.c	Find Maximum & Minimum in Array	Arrays, Comparison
+19	19_ArrayEvenOddCount.c	Count Even & Odd Elements	Arrays, if-else
+20	20_ArrayReverse.c	Reverse an Array	Arrays, Loops
+21	21_ArraySecondLargest.c	Find Second Largest Element	Arrays, Comparison
+22	22_MergeTwoArrays.c	Merge Two Arrays	Arrays
+23	23_CopyArray.c	Copy One Array to Another	Arrays
+24	24_MatrixAddition.c	Matrix Addition	2D Arrays
+25	25_MatrixTranspose.c	Matrix Transpose	2D Arrays
+26	26_MatrixMultiplication.c	Matrix Multiplication	2D Arrays
+27	27_StringLength.c	Find String Length (Without strlen())	Strings
+28	28_StringReverse.c	Reverse a String	Strings
+29	29_PalindromeString.c	Check Palindrome String	Strings
+30	30_VowelConsonantDigitSpecial.c	Count Vowels, Consonants, Digits & Special Characters	Strings, Character Handling
+No.	File Name	Topic
+31	31_FunctionWithoutArgument.c	Function Without Arguments
+32	32_FunctionWithArgument.c	Function With Arguments
+33	33_FunctionReturnValue.c	Function with Return Value
+34	34_RecursiveFactorial.c	Recursion
+35	35_RecursiveFibonacci.c	Recursive Fibonacci
+36	36_PointerBasics.c	Pointer Basics
+37	37_PointerArithmetic.c	Pointer Arithmetic
+38	38_PointerAndArray.c	Pointer with Arrays
+39	39_SwapUsingPointers.c	Swap Using Pointers
+40	40_StringUsingPointers.c	String with Pointers
