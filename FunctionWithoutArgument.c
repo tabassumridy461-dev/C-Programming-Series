45e4d5a36eacc877336e2012cef34ae16164f938
Tabassum Ridy <tabassumridy461@gmail.com>
2026-08-02 23:47:20 +0600

    Create FunctionWithoutArgument.c

---
+++ FunctionWithoutArgument.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void displayMessage();
+
+int main()
+{
+    displayMessage();
+    return 0;
+}
+
+void displayMessage()
+{
+    printf("Welcome to C Programming!\n");
+    printf("This is a function without arguments.\n");
+}
